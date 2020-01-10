fdb38bca6c4b7d63af7fbb5138fa0f9b7e7bf0c8
Mos Zheng <47658172+moszheng@users.noreply.github.com>
2020-01-10 18:28:57 +0800

    Add files via upload
    V0.9 
    目前只有醜醜的介面，還沒有GUI，預計下一版導入GUI+Clipboard+WintoMac變換。

---
+++ JL_Nas_translator_V0.9.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include<string.h>
+using namespace std;
+int main()
+{
+	char jl[300];
+	int lenthstr,lenth;
+	
+	cout<<"**********************"<<endl<<endl;
+	cout<<"貼上 Mac 路徑 : "<<endl<<endl;
+	
+	cin.getline(jl,300); //純cin吃到空格就會終止
+	lenth = strlen(jl);
+	
+	cout<<endl<<endl<< "**********************"<<endl;
+	cout<<endl<<"Windows 路徑 : "<<endl<<endl<<endl<<"\\\\JLDESIGNTPE";
+	
+	for (int i=40 ; i < lenth ; i++){
+	
+		if (jl[i] == '/' ){
+
+			cout << "\\" ;
+		}
+
+		else 
+		cout << jl[i] ;
+   }
+	
+	cout<<endl<<endl<<endl<<"**********************"<<endl<<endl<<endl;
+	system ("pause"	);
+	
+}
